9bd91b1c5d4c849ffdafc2c76ed017bd02325fec
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2023-10-09 22:54:05 -0300

    update module: model

--- html.go
+++ html.go
@@ -1,28 +1,27 @@
 package datalistplus
 
+// <div onclick="selOptionDLPlus(this)" class="option-dlplus-checked">
+//   <input type="radio" class="dlplus-selected-radio" id="automobiles" name="category" />
+//   <label data-id="111" data-description="auto" for="automobiles">Automobiles</label>
+// </div>
+
+// <div onclick="selOptionDLPlus(this)" class="option-dlplus-checked">
+//   <input type="radio" class="dlplus-selected-radio" id="film" name="category" />
+//   <label data-id="222" data-description="anime" for="film">Film & Animation</label>
+// </div>
+
+// <div onclick="selOptionDLPlus(this)" class="option-dlplus-checked">
+//   <input type="radio" class="dlplus-selected-radio" id="science" name="category" />
+//   <label data-id="333" data-description="ciencia" for="science">Science & Technology<span class="code-righ-name">obs:tv</span></label>
+// </div>
+
 func (d DataListPlus) HtmlContainer() string {
 
 	return `<div class="dlplus-container">
 	<div id="dlplus-options-container" data-id="` + d.ObjectVIEW().Name + `">
 
-	  <div onclick="selOptionDLPlus(this)" class="option-dlplus-checked">
-		<input type="radio" class="dlplus-selected-radio" id="automobiles" name="category" />
-		<label data-id="111" data-description="auto" for="automobiles">Automobiles</label>
+	
 	  </div>
-
-	  <div onclick="selOptionDLPlus(this)" class="option-dlplus-checked">
-		<input type="radio" class="dlplus-selected-radio" id="film" name="category" />
-		<label data-id="222" data-description="anime" for="film">Film & Animation</label>
-	  </div>
-
-	  <div onclick="selOptionDLPlus(this)" class="option-dlplus-checked">
-		<input type="radio" class="dlplus-selected-radio" id="science" name="category" />
-		<label data-id="333" data-description="ciencia" for="science">Science & Technology<span class="code-righ-name">obs:tv</span></label>
-	  </div>
-	</div>
-
-
-
 
 	<div class="dlplus-selected" onclick="newSelectionDLPlus(this)">
 	  <svg aria-hidden="true" focusable="false" class="dlplus-icon-arrow"><use xlink:href="#icon-arrow-down" /></svg>
@@ -30,11 +29,11 @@ func (d DataListPlus) HtmlContainer() string {
 	 <i class="dlplus-title" >Selección</i>
 	</div>
 
-
 	<div name="search-dlplus-box">
-	  <input type="search" name="search" placeholder="Search" onkeyup="searchOptionDLPlus(this)" />
+	  <input type="search" name="search" placeholder="⚲" onkeyup="searchOptionDLPlus(this)" />
 	</div>
-  </div>`
+
+	</div>`
 }
 
 func (d DataListPlus) BuildTag() string {

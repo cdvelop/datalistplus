58380667447f3a51d83fd03ae6b1ec9bbea9c551
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2023-10-11 20:54:59 -0300

    update module: model

---
+++ model.go
@@ -0,0 +1,19 @@
+package datalistplus
+
+import "github.com/cdvelop/model"
+
+type DataListPlus struct {
+	Object *model.Object // ej: patient
+
+	FieldID string // ej: "id_client"
+
+	FieldText string // ej: "client_name"
+
+	FieldDescription string // ej: "description"
+
+	FieldFooter string // ej: "value"
+
+	FieldExtra string // ej: "dto"
+
+	FieldStatus string // ej: "state" (checked, viewed)
+}
